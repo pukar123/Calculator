3f877a7acf4a166519652a16b02f71ceb3c17877
pukarsubedi18 <pukarsubedi18@gmail.com>
2023-04-05 09:00:05 +1000

    Error Message have been modified

--- CalculatorAPI/Controllers/CalculatorController.cs
+++ CalculatorAPI/Controllers/CalculatorController.cs
@@ -15,31 +15,77 @@ namespace CalculatorAPI.Controllers
 
         public CalculatorController(ICalculatorService calculatorService)
         {
-            _calculatorService = calculatorService;
+                _calculatorService = calculatorService;  
         }
+        //Takes two values as input and shows their sum as output
 
         [HttpGet("add")]
         public ActionResult<decimal> Add([FromQuery] CalculatorDTO calc)
         {
-            return _calculatorService.Add(calc);
-        }
+            try
+            {
+                return Ok(_calculatorService.Add(calc));
+            }
+            catch (Exception ex)
+            {
 
+                return BadRequest(new ProblemDetails
+                {
+                    Title = "Something went wrong",
+                    Detail = $"{ex.Message}"
+                });
+            }
+        }
+        //Takes two values as input and shows their difference as output
         [HttpGet("subtract")]
         public ActionResult<decimal> Subtract([FromQuery] CalculatorDTO calc)
         {
-            return _calculatorService.Subtract(calc);
-        }
+            try
+            {
+                return Ok(_calculatorService.Subtract(calc));
+            }
+            catch (Exception ex)
+            {
 
+                return BadRequest(new ProblemDetails
+                {
+                    Title = "Something went wrong",
+                    Detail = $"{ex.Message}"
+                });
+            }
+        }
+        //Takes two values as input and shows their Multiplication as output
         [HttpGet("multiply")]
         public ActionResult<decimal> Multiply([FromQuery] CalculatorDTO calc)
         {
-            return _calculatorService.Multiply(calc);
-        }
+            try
+            {
+                return Ok(_calculatorService.Multiply(calc));
+            }
+            catch (Exception ex)
+            {
 
+                return BadRequest(new ProblemDetails
+                {
+                    Title = "Something went wrong",
+                    Detail = $"{ex.Message}"
+                });
+            }
+        }
+        //Takes two values as input and shows result as first value divided by second value
         [HttpGet("divide")]
         public ActionResult<decimal> Divide([FromQuery] CalculatorDTO calc)
         {
-            return _calculatorService.Divide(calc);
+            try
+            {
+                return Ok(_calculatorService.Divide(calc));
+            }
+            catch (Exception ex)
+            { 
+                return BadRequest(new ProblemDetails { 
+                    Title = "Something went wrong", 
+                    Detail = $"{ex.Message}" }); 
+            }
         }
     }
 }
